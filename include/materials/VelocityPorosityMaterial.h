c385989da35f3ed6dea09918cf3b02940713c904
wang zhaohao <wangzhaohao@wangs-MacBook-Air.local>
2022-12-29 16:59:48 +0800

    2022.12.28_mac

--- include/materials/VelocityPorosityMaterial.h
+++ include/materials/VelocityPorosityMaterial.h
@@ -1,19 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
 #pragma once
 
 #include "Material.h"
 
-// class Function;
-
 class VelocityPorosityMaterial : public Material
 {
-  public:
-    static InputParameters validParams();
+public:
+  VelocityPorosityMaterial(const InputParameters & parameters);
 
-    VelocityPorosityMaterial(const InputParameters & parameters);
-  protected:
-    virtual void computeProperties();
+  static InputParameters validParams();
 
-    ADMaterialProperty<Real> & _velocity_pore;
-    const VariableValue & _temperature;
-    const VariableGradient & _grad_temperature;
+protected:
+  virtual void computeProperties() override;
+
+private:
+
+  ADMaterialProperty<Real> & _velocity_pore;
+  const VariableValue & _temperature;
+  const VariableGradient & _grad_temperature;
 };
